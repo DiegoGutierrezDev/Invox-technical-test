40ffed40536b5db7f8c9940ddbb470adeceaaa48
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-07-15 16:21:58 +0200

    Improving components

--- frontend/components/AppButton.vue
+++ frontend/components/AppButton.vue
@@ -1,7 +1,13 @@
 <template>
   <button
-    class="bg-purple rounded-lg px-6 py-4 h-8 flex items-center justify-center"
+    class="bg-purple rounded-lg px-6 py-4 h-8 flex items-center justify-center text-white font-semibold"
   >
-    <slot name="content"></slot>
+    {{ text }}
   </button>
 </template>
+
+<script setup lang="ts">
+const { text } = defineProps<{
+  text: string;
+}>();
+</script>
