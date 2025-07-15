ef696b4f5f1c972ab5e25d765549a820fcde9f6a
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-07-15 16:29:39 +0200

    front: Creating index and global css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/tailwind.css'],
 })
